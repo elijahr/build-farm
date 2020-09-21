2c685e5ab951b4077045fda5965a223f66a3a7ea
Elijah Shaw-Rutschman <elijahr@gmail.com>
2020-09-20 21:43:56 -0500

    Swap

--- scripts/distcc-client-archlinux/create-symlinks.sh
+++ scripts/distcc-client-archlinux/create-symlinks.sh
@@ -3,11 +3,11 @@
 set -uxeo pipefail
 
 # ccache and distcc provides gcc/g++ wrappers, but not cc, so create additional symlinks
-if [[ ! -f /usr/lib/distcc/cc ]]
+if [[ ! -f /usr/lib/distcc/bin/cc ]]
 then
-  ln -s /usr/bin/distcc /usr/lib/distcc/cc
+  ln -s /usr/bin/distcc /usr/lib/distcc/bin/cc
 fi
-if [[ ! -f /usr/lib/ccache/cc ]]
+if [[ ! -f /usr/lib/ccache/bin/cc ]]
 then
-  ln -s /usr/bin/ccache /usr/lib/ccache/cc
+  ln -s /usr/bin/ccache /usr/lib/ccache/bin/cc
 fi
--- scripts/distcc-client-debian/create-symlinks.sh
+++ scripts/distcc-client-debian/create-symlinks.sh
@@ -3,11 +3,11 @@
 set -uxeo pipefail
 
 # ccache and distcc provides gcc/g++ wrappers, but not cc, so create additional symlinks
-if [[ ! -f /usr/lib/distcc/bin/cc ]]
+if [[ ! -f /usr/lib/distcc/cc ]]
 then
-  ln -s /usr/bin/distcc /usr/lib/distcc/bin/cc
+  ln -s /usr/bin/distcc /usr/lib/distcc/cc
 fi
-if [[ ! -f /usr/lib/ccache/bin/cc ]]
+if [[ ! -f /usr/lib/ccache/cc ]]
 then
-  ln -s /usr/bin/ccache /usr/lib/ccache/bin/cc
+  ln -s /usr/bin/ccache /usr/lib/ccache/cc
 fi
