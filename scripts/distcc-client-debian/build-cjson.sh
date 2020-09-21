fcb85818a644f303a22a91a8b57e26c493ef2998
Elijah Shaw-Rutschman <elijahr@gmail.com>
2020-09-20 20:22:44 -0500

    archlinux

---
+++ scripts/distcc-client-debian/build-cjson.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -uxeo pipefail
+
+main () {
+  cd $(dirname $0)
+
+  # gcc/etc should use ccache first
+  test $(which gcc) == "/usr/lib/ccache/gcc"
+  test $(which g++) == "/usr/lib/ccache/g++"
+  test $(which cc) == "/usr/lib/ccache/cc"
+
+  # Assert that ccache wrappers wrap distcc wrappers
+  [[ "$((gcc 2>&1 || true) | tail -n 1)" =~ ^distcc[\[0-9\]+] ]]
+
+  # Compile cJSON, will make two requests to distcc-host
+  rm -Rf /tmp/cJSON
+  mkdir /tmp/cJSON
+  tar xzf test-data/cJSON-master.tar.gz -C /tmp/cJSON
+  cd /tmp/cJSON/cJSON-master
+  make
+  make test
+
+  # Run make again, should use ccached object files instead of making requests to distcc-host
+  make clean
+  make
+  make test
+}
+
+main
--- scripts/render-templates.py
+++ scripts/render-templates.py
@@ -7,13 +7,12 @@ Generate various docker and config files across a matrix of archs/distros.
 import os
 import re
 
-host_debian_distros = ( 'debian:buster', )
-host_archs = ( 'amd64', )
-
-client_debian_distros = ( 'debian:buster', )
-client_archs = ( 'amd64', 'i386', 'arm32v7', 'arm64v8', 'ppc64le', 's390x' )
-
-ports_by_arch = {
+# Debian
+debian_host_distros = ( 'debian:buster', )
+debian_host_archs = ( 'amd64', )
+debian_client_distros = ( 'debian:buster', )
+debian_client_archs = ( 'amd64', 'i386', 'arm32v7', 'arm64v8', 'ppc64le', 's390x' )
+debian_ports_by_arch = {
   'amd64': 3632,
   'i386': 3633,
   'arm32v7': 3634,
@@ -21,8 +20,7 @@ ports_by_arch = {
   'ppc64le': 3636,
   's390x': 3637,
 }
-
-toolchains_by_arch = {
+debian_toolchains_by_arch = {
   'amd64': 'x86_64-linux-gnu',
   'i386': 'i686-linux-gnu',
   'arm32v7': 'arm-linux-gnueabihf',
@@ -30,8 +28,7 @@ toolchains_by_arch = {
   'ppc64le': 'powerpc64le-linux-gnu',
   's390x': 's390x-linux-gnu',
 }
-
-flags_by_arch = {
+debian_flags_by_arch = {
   'amd64': 'START_DISTCC_X86_64_LINUX_GNU',
   'i386': 'START_DISTCC_I686_LINUX_GNU',
   'arm32v7': 'START_DISTCC_ARM_LINUX_GNUEABIHF',
@@ -40,10 +37,39 @@ flags_by_arch = {
   's390x': 'START_DISTCC_S390X_LINUX_GNU',
 }
 
-def get_compiler_path_part_by_arch(arch):
+
+# Arch Linux
+archlinux_host_archs = ( 'amd64', )
+archlinux_client_archs = ( 'amd64', 'arm32v6', 'arm32v7', 'arm64v8', )
+archlinux_ports_by_arch = {
+  'amd64': 3732,
+  'arm32v6': 3733,
+  'arm32v7': 3734,
+  'arm64v8': 3735,
+}
+archlinux_images_by_arch = {
+  'amd64': 'archlinux:20200908',
+  'arm32v6': 'lopsided/archlinux:109729d4d863e14fed6faa1437f0eaee8133b26c310079c8294a4c7db6dbebb5',
+  'arm32v7': 'lopsided/archlinux:fbf2d806f207a2e9a5400bd20672b80ca318a2e59fc56c1c0f90b4e9adb60f4a',
+  'arm64v8': 'lopsided/archlinux:f9d68dd73a85b587539e04ef26b18d91b243bee8e1a343ad97f67183f275e548'
+}
+archlinux_toolchains_by_arch = {
+  'arm32v6': 'x-tool6h/arm-unknown-linux-gnueabihf',
+  'arm32v7': 'x-tool7h/arm-unknown-linux-gnueabihf',
+  'arm64v8': 'x-tool8/aarch64-unknown-linux-gnu',
+}
+
+
+def get_debian_compiler_path_part_by_arch(arch):
   if arch == 'amd64':
     return ''
-  return '/usr/local/'+toolchains_by_arch[arch]+'/bin:'
+  return '/usr/local/'+debian_toolchains_by_arch[arch]+'/bin:'
+
+
+def get_archlinux_compiler_path_part_by_arch(arch):
+  if arch == 'amd64':
+    return ''
+  return '/root/'+archlinux_toolchains_by_arch[arch]+'/bin:'
 
 
 def slugify(string):
@@ -65,35 +91,65 @@ def render(template, out, context):
 
 
 def generate_host_dockerfiles():
-  for arch in host_archs:
-    for distro in host_debian_distros:
-      distro_slug = slugify(distro)
+  for host_arch in debian_host_archs:
+    for host_distro in debian_host_distros:
+      host_distro_slug = slugify(host_distro)
       render(
         'Dockerfile.distcc-host.debian.template',
-        'Dockerfile.distcc-host.{distro_slug}.{arch}',
+        'Dockerfile.distcc-host.{host_distro_slug}.{host_arch}',
         locals(),
       )
+
+  for host_arch in archlinux_host_archs:
+    host_image = archlinux_images_by_arch[host_arch]
+    render(
+      'Dockerfile.distcc-host.archlinux.template',
+      'Dockerfile.distcc-host.archlinux.{host_arch}',
+      locals(),
+    )
 
 
 def generate_client_dockerfiles():
-  for arch in client_archs:
-    for distro in client_debian_distros:
-      distro_slug = slugify(distro)
-      host_port = ports_by_arch[arch]
+  for client_arch in debian_client_archs:
+    for client_distro in debian_client_distros:
+      host_distro_slug = slugify(client_distro)
+      host_port = debian_ports_by_arch[client_arch]
       render(
         'Dockerfile.distcc-client.debian.template',
-        'Dockerfile.distcc-client.{distro_slug}.{arch}',
+        'Dockerfile.distcc-client.{host_distro_slug}.{host_arch}',
         locals(),
       )
 
+  for client_arch in archlinux_client_archs:
+    client_image = archlinux_images_by_arch[client_arch]
+    host_port = debian_ports_by_arch[client_arch]
+    render(
+      'Dockerfile.distcc-client.archlinux.template',
+      'Dockerfile.distcc-client.archlinux.{host_arch}',
+      locals(),
+    )
+
 
 def generate_docker_compose():
-  for host_arch in host_archs:
-    for host_distro in host_debian_distros:
+  for host_arch in debian_host_archs:
+    for host_distro in debian_host_distros:
       host_distro_slug = slugify(host_distro)
-      for client_arch in client_archs:
-        host_port = ports_by_arch[client_arch]
-        for client_distro in client_debian_distros:
+      for client_arch in debian_client_archs:
+        host_port = debian_ports_by_arch[client_arch]
+        for client_distro in debian_client_distros:
+          client_distro_slug = slugify(client_distro)
+          render(
+            'docker-compose.template.yml',
+            'docker-compose.host-{host_distro_slug}-{host_arch}.client-{client_distro_slug}-{client_arch}.yml',
+            locals()
+          )
+
+  for host_arch in archlinux_host_archs:
+    for host_distro in host_archlinux_distros:
+      host_distro_slug = slugify(host_distro)
+      for client_arch in archlinux_client_archs:
+        host_port = archlinux_ports_by_arch[client_arch]
+        for client_distro in client_archlinux_distros:
           client_distro_slug = slugify(client_distro)
           render(
             'docker-compose.template.yml',
@@ -103,25 +159,40 @@ def generate_docker_compose():
 
 
 def generate_distccd_config():
-  for arch in client_archs:
-    flag = flags_by_arch[arch]
-    toolchain = toolchains_by_arch[arch]
-    host_port = ports_by_arch[arch]
-    compiler_path_part = get_compiler_path_part_by_arch(arch)
+  for client_arch in debian_client_archs:
+    flag = debian_flags_by_arch[arch]
+    toolchain = debian_toolchains_by_arch[client_arch]
+    host_port = debian_ports_by_arch[client_arch]
+    compiler_path_part = get_debian_compiler_path_part_by_arch(client_arch)
 
     render(
-      'etc/default/distccd-template',
-      'etc/default/distccd-{toolchain}',
+      'distcc-host-debian/etc/default/distccd-template',
+      'distcc-host-debian/etc/default/distccd-{toolchain}',
       locals(),
     )
     render(
-      'etc/init.d/distccd-template',
-      'etc/init.d/distccd-{toolchain}',
+      'distcc-host-debian/etc/init.d/distccd-template',
+      'distcc-host-debian/etc/init.d/distccd-{toolchain}',
       locals(),
     )
     render(
-      'etc/logrotate.d/distccd-template',
-      'etc/logrotate.d/distccd-{toolchain}',
+      'distcc-host-debian/etc/logrotate.d/distccd-template',
+      'distcc-host-debian/etc/logrotate.d/distccd-{toolchain}',
+      locals(),
+    )
+
+  for client_arch in archlinux_client_archs:
+    host_port = archlinux_ports_by_arch[client_arch]
+    compiler_path_part = get_archlinux_compiler_path_part_by_arch(client_arch)
+
+    render(
+      'distcc-host-archlinux/etc/conf.d/distccd-template',
+      'distcc-host-archlinux/etc/conf.d/distccd-{client_arch}',
+      locals(),
+    )
+    render(
+      'distcc-host-archlinux/usr/lib/systemd/system/distccd.service-template',
+      'distcc-host-archlinux/usr/lib/systemd/system/distccd-{client_arch}.service',
       locals(),
     )
 
