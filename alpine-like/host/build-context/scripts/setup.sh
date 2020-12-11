0863e1ad8740d17250ec3d9d348bc68b3c485e50
Elijah Shaw-Rutschman <elijahr@gmail.com>
2020-12-11 02:22:32 -0600

    Proper symlinking

--- alpine-like/host/build-context/scripts/setup.sh
+++ alpine-like/host/build-context/scripts/setup.sh
@@ -20,5 +20,31 @@ then
       rm $file
     fi
   done
+  for dir in *
+  do
+    if [ "$dir" != "*" ]
+    then
+      cd ${dir}/bin
+      for file in *
+      do
+        if [ "$file" != "*" ]
+        then
+          # symlink toolchain binaries for whitelisting
+          if [ ! -f "/usr/lib/distcc/${file}" ]
+          then
+            ln -s "$(readlink -f $file)" "/usr/lib/distcc/${file}"
+            # alias <arch>-linux-musl-<cmd> as <arch>-alpine-linux-musl-<cmd>
+            ln -s "$(readlink -f $file)" "$(dirname $(readlink -f $file))/$(echo $file | sed 's/linux/alpine-linux/')"
+          fi
+          if [ ! -f "/usr/lib/distcc/$(echo $file | sed 's/linux-musl/alpine-linux-musl/')" ]
+          then
+            # alias <arch>-linux-musl-<cmd> as <arch>-alpine-linux-musl-<cmd>
+            ln -s "$(readlink -f $file)" "/usr/lib/distcc/$(echo $file | sed 's/linux/alpine-linux/')"
+          fi
+        fi
+      done
+      cd -
+    fi
+  done
   cd -
 fi
